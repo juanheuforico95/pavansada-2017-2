e84243e63e05c40589bc8fcb1563c8635a424b07
juanheuforico95 <juanheuforico@gmail.com>
2017-09-06 20:07:24 -0500

    mini proy
    mini proy

--- miniproy/maxones.c
+++ miniproy/maxones.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 #include "maxones.h"
-//int flag=1;
-  //  int input[]={1,1,0,1};
 
+/*
+*la funcion findMaxConsecutiveOnes sirve para contar cuantos 1
+* hay consecutivamente, recive de parametros un arreglo con los numeros
+* y el tamaño del arreglo para poder navegar atraves de el.
+**nums: arreglo con los valores
+**numsSize: tamaño del arreglo
+*/
     int findMaxConsecutiveOnes(int* nums, int numsSize){
-      //int input[]=nums;
       //estos son contadores
       int numones=0;
       int maxones=0;
-      //sirve para ver si el arreglo contiene solo unos
+      //flag sirve para que se guarde el maxones si no se encuentra un 0
       int flag=1;
     for(int i = 0; i < numsSize; i++){
         if(nums[i]== 1){
@@ -22,13 +26,19 @@
                  flag=0;
              }
             numones=0;
-            // printf("posicion %d\n", posblank);
         }}
-        //calcula cuantos 1 hay
+        //calcula cuantos 1 seguidos hay
         maxones=(maxones+(numones*flag));
         printf("el maximo numero de unos consecutivos es:  %d\n", maxones);
       return maxones;
     }
+    /*
+    esta funcion sirve para ver si los valores en el arreglos son
+    diferentes de 0 y 1;
+    parametros:
+    *nums: arreglo con los valores
+    *numsSize: tamaño del arreglo
+    */
       int CheckInput(int* nums, int numsSize){
         for(int i = 0; i < numsSize; i++){
           if(nums[i]!= 1 && nums[i]!=0){
