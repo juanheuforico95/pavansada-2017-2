4f305e08e32f6dff4bf19f8962e5c0cba08144cf
juanheuforico95 <juanheuforico@gmail.com>
2017-11-09 18:55:08 -0600

    correccion hilo
    correccion hilo

--- thread/main.c
+++ thread/main.c
@@ -1,31 +1,56 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <pthread.h>
-#include "hellow.h"
-#define NUM_THREADS 5
-/* Add function signatures here */
-void *PrintHello(void *threadid)
- {
-    long tid;
-    tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
-    pthread_exit(NULL);
- }
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#define NUM_THREADS	10
 
- int main (int argc, char *argv[])
- {
+sem_t forks[NUM_THREADS];
+
+void thinking(int n) {
+    sleep(rand()%n);
+}
+
+void eating(int n) {
+    sleep(rand()%n);
+}
+
+void *lifestyle(void *threadid) {
+    long tid;
+    tid = (long) threadid;
+    while(1) {
+        thinking(3);
+        printf("I am philosopher %lu, I am hungry\n", tid);
+        sem_wait(&forks[tid]);
+        sem_wait(&forks[(tid + 1) % NUM_THREADS]);
+        eating(4);
+        sem_post(&forks[tid]);
+        sem_post(&forks[(tid + 1) % NUM_THREADS]);
+        printf("I am philosopher %lu, I am thinking\n", tid);
+    }
+}
+
+int main(int argc, char ** argv) {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
-    for(t=0; t<NUM_THREADS; t++){
-       printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-       if (rc){
-          printf("ERROR; return code from pthread_create() is %d\n", rc);
-          exit(-1);
-       }
+
+    for (t = 0; t < NUM_THREADS; t++) {
+        sem_init(&forks[t], 0, 1);
     }
 
-    /* Last thing that main() should do */
+    for (t=0; t < NUM_THREADS; t++) {
+        printf("In main: creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, lifestyle, (void *)t);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    for (t = 0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
+    }
+
     pthread_exit(NULL);
- }
+}
